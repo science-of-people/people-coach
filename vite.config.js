d0f27d5e8632974877b719ee7cb23de48f4f2a48
scott-edwards <scott.t.edwards@gmail.com>
2025-12-01 12:53:01 -0800

    Add Terms of Service page and link in footer

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
+                terms: resolve(__dirname, 'terms.html'),
             },
         },
     },
